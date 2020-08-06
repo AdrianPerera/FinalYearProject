d6771dad138029a5eddf73983e9517079895c52a
AdrianPerera <nikiperera20@gmail.com>
2020-08-06 17:27:54 +0530

    FIX : refactored paths to tabs

--- src/components/DrawerContent.js
+++ src/components/DrawerContent.js
@@ -27,68 +27,8 @@ function DrawerContent({...props}) {
   }
 
   return (
-    <DrawerContentScrollView>
-      <View style={{}}>
-        <DrawerItem
-          icon={({focused, color, size}) => (
-            <Icon
-              style={{marginRight: -3}}
-              name="list-alt"
-              size={size}
-              color={focused ? '#3eb708' : '#442b2b'}
-            />
-          )}
-          label="Details"
-          onPress={() => {
-            props.navigation.navigate('landingTab');
-          }}
-        />
-
-        <DrawerItem
-          icon={({focused, size}) => (
-            <Icon
-              style={{marginRight: 3, marginLeft: 3}}
-              name="user"
-              size={size}
-              color={focused ? '#3eb708' : '#442b2b'}
-            />
-          )}
-          label="Profile"
-          onPress={() => {
-            props.navigation.navigate('profileTab');
-          }}
-        />
-
-        <DrawerItem
-          icon={({focused, size}) => (
-            <Icon
-              style={{marginRight: -3}}
-              name="envelope"
-              size={size}
-              color={focused ? '#3eb708' : '#442b2b'}
-            />
-          )}
-          label="Messages"
-          onPress={() => {
-            props.navigation.navigate('messagesTab');
-          }}
-        />
-
-        <DrawerItem
-          icon={({focused, size}) => (
-            <Icon
-              style={{marginRight: -3}}
-              name="money"
-              size={size}
-              color={focused ? '#3eb708' : '#442b2b'}
-            />
-          )}
-          label="Payments"
-          onPress={() => {
-            props.navigation.navigate('paymentsTab');
-          }}
-        />
-      </View>
+    <DrawerContentScrollView {...props}>
+      <DrawerItemList {...props} />
 
       <View style={{position:'relative'}}>
       <DrawerItem
--- src/scenes/ProfileScene.js
+++ src/scenes/ProfileScene.js
@@ -1,45 +1,75 @@
 import React, {Component} from 'react';
-import {createDrawerNavigator,DrawerItem} from '@react-navigation/drawer';
-import MessagesTab from '../tabs/MessagesTab';
-import PaymentTab from '../tabs/PaymentTab';
-import ProfileTab from '../tabs/ProfileTab';
-import LandingTab from '../tabs/LandingTab';
+import {StyleSheet, View} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import MessagesTab from '../tabs/patient/MessagesTab';
+import PaymentTab from '../tabs/patient/PaymentTab';
+import ProfileTab from '../tabs/patient/ProfileTab';
+import LandingTab from '../tabs/patient/LandingTab';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import DrawerContent from '../components/DrawerContent';
 
-function ProfileScene({route}) {
-  const Drawer = createDrawerNavigator();
-  const {param} =route.params.params;
-
-  if(param.selected==='patient'){
+function ProfileScene() {
+    const Drawer = createDrawerNavigator();
     return (
-    
-      <Drawer.Navigator 
-        minSwipeDistance={70}
-        initialRouteName="landingTab"
-        drawerContent={props => <DrawerContent {...props}/>}
-        >
-        <Drawer.Screen name="landingTab" component={LandingTab} />
-        <Drawer.Screen name="profileTab" component={ProfileTab} />
-        <Drawer.Screen name="messagesTab" component={MessagesTab} />
-        <Drawer.Screen name="paymentsTab" component={PaymentTab} />
-      
-      </Drawer.Navigator>
-        
-      );
-    }
-  
-  if(param.selected==='doctor'){
-    return ( 
-      null
-      //Navigator to return if Doctor is logged in
+        <Drawer.Navigator
+            drawerContent={props => <DrawerContent {...props} />}
+            backBehavior="history"
+            drawerType="front"
+            minSwipeDistance={70}
+            initialRouteName="landingTab">
+            <Drawer.Screen name="landingTab" component={LandingTab}
+                           options={{
+                               title: 'Profile Details',
+                               drawerIcon: ({focused, size}) => (
+                                   <Icon style={{marginRight: -3}} name="list-alt" size={size}
+                                         color={focused ? '#4544ca' : '#442b2b'}
+                                   />
+                               ),
+                           }}
+            />
+            <Drawer.Screen name="profileTab" component={ProfileTab} options={{
+                title: ' My Profile', drawerIcon: ({focused, size}) => (
+                    <Icon
+                        style={{marginRight: -3, marginLeft: 3}}
+                        name="user"
+                        size={size}
+                        color={focused ? '#4544ca' : '#442b2b'}
+                    />
+                ),
+            }}
+            />
+            <Drawer.Screen
+                name="messagesTab"
+                component={MessagesTab}
+                options={{
+                    title: 'Messages',
+                    drawerIcon: ({focused, size}) => (
+                        <Icon
+                            style={{marginRight: -3}}
+                            name="envelope-o"
+                            size={size}
+                            color={focused ? '#4544ca' : '#442b2b'}
+                        />
+                    ),
+                }}
+            />
+            <Drawer.Screen
+                name="paymentsTab"
+                component={PaymentTab}
+                options={{
+                    title: 'Payments',
+                    drawerIcon: ({focused, size}) => (
+                        <Icon
+                            style={{marginRight: -4}}
+                            name="money"
+                            size={size}
+                            color={focused ? '#4544ca' : '#442b2b'}
+                        />
+                    ),
+                }}
+            />
+        </Drawer.Navigator>
     );
-  }
-  if(param.selected==='caretaker'){
-    return (null
-      //Navigatro to return if caretaker is logged in 
-      );
-  }
-  
 }
 
 export default ProfileScene;
