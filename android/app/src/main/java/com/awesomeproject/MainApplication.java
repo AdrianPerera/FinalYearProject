e0017d2de5ee4f816476621c399d6f6d5431bba3
AdrianPerera <nikiperera20@gmail.com>
2020-09-02 19:38:30 +0530

    React-native manually upgraded with firebase integration

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -1,4 +1,4 @@
-package com.awesomeproject;
+package com.finalyearproject;
 
 import android.app.Application;
 import android.content.Context;
@@ -64,7 +64,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.awesomeproject.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.finalyearproject.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
