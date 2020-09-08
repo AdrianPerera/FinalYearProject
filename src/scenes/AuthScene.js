790540c2468cc67720765d83dafcccb3ffb1af9b
AdrianPerera <nikiperera20@gmail.com>
2020-09-08 17:55:42 +0530

    NIT : changed the location of the getFCM token method. needs fixing

--- src/scenes/AuthScene.js
+++ src/scenes/AuthScene.js
@@ -3,84 +3,9 @@ import { Text }  from 'react-native';
 import {Picker, Form, Item, Button, Container, Input, Label} from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import {styles} from '../styles/AuthScStyles';
-import AsyncStorage from '@react-native-community/async-storage';
-import firebase from "react-native-firebase";
-
-
 
 export default class AuthScene extends Component {
 
-  async componentDidMount() {
-    //we check if user has granted permission to receive push notifications.
-    this.checkPermission();
-    // Register all listener for notification 
-    this.createNotificationListeners();
-  } 
-    async checkPermission() {
-    const enabled = await firebase.messaging().hasPermission();
-
-    // If Premission granted proceed towards token fetch
-    if (enabled) {
-      this.getToken();
-    } else {
-      // If permission hasn’t been granted to our app, request user in requestPermission method. 
-      this.requestPermission();
-    }
-  }
-
-  async getToken() {
-    let fcmToken = await AsyncStorage.getItem('fcmToken');
-    if (!fcmToken) {
-      fcmToken = await firebase.messaging().getToken();
-    
-      if (fcmToken) {
-        // user has a device token
-        await AsyncStorage.setItem('fcmToken', fcmToken);
-      }
-    }
-  }
- 
-  async requestPermission() {
-    try {
-      await firebase.messaging().requestPermission();
-      // User has authorised
-      this.getToken();
-    } catch (error) {
-      // User has rejected permissions
-      console.log('permission rejected');
-    }
-  }
-
-  async createNotificationListeners() {
-
-    // This listener triggered when notification has been received in foreground
-    this.notificationListener = firebase.notifications().onNotification((notification) => {
-      const { title, body } = notification;
-      this.displayNotification(title, body);
-    });
-
-    // This listener triggered when app is in backgound and we click, tapped and opened notifiaction
-    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
-      const { title, body } = notificationOpen.notification;
-      this.displayNotification(title, body);
-    });
-
-    // This listener triggered when app is closed and we click,tapped and opened notification 
-    const notificationOpen = await firebase.notifications().getInitialNotification();
-    if (notificationOpen) {
-      const { title, body } = notificationOpen.notification;
-      this.displayNotification(title, body);
-    }
-  }
-
-  displayNotification = (title, body)=> {
-    // we display notification in alert box with title and body
-    console.log(title);
-    console.log(body);
-    }
-
-
- 
   state = {
     selected: 'patient',
     username: '',
--- src/scenes/SignUpScene.js
+++ src/scenes/SignUpScene.js
@@ -20,20 +20,62 @@ import {
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import {styles} from '../styles/SignUpStyles';
 import AsyncStorage from '@react-native-community/async-storage';
+import firebase from 'react-native-firebase';
 
 class SignUpScene extends Component {
+  // async componentDidMount() {
+  //   //we check if user has granted permission to receive push notifications.
+  //   this.checkPermission();
+  // }
+
+   componentDidMount() {
+    const enabled = await firebase.messaging().hasPermission();
+
+    // If Premission granted proceed towards token fetch
+    if (enabled) {
+      this.getToken();
+    } else {
+      // If permission hasn’t been granted to our app, request user in requestPermission method.
+      this.requestPermission();
+    }
+   }
+
+  // async requestPermission() {
+  //   try {
+  //     await firebase.messaging().requestPermission();
+  //     // User has authorised
+  //     this.getToken();
+  //   } catch (error) {
+  //     // User has rejected permissions
+  //     console.log('permission rejected');
+  //   }
+  // }
+
   state = {
+    secureEntry: true,
+    mosdalVisible: false,
     selected: undefined,
     checked: false,
-    secureEntry: true,
-    modalVisible: false,
     fcmToken: '',
+    username: '',
+    phoneNumber: '',
+    email: '',
+    password: '',
+    reEnterPassword: '',
   };
 
-  async getToken() {
-    var fcmToken = AsyncStorage.getItem('fcmToken');
-    this.setState({fcmToken:fcmToken});
-  } 
+  // async getToken() {
+  //   var fcmToken = AsyncStorage.getItem('fcmToken').then((value) =>
+  //     this.setState({fcmToken: value}),
+  //   );
+  //   if (!fcmToken) {
+  //     fcmToken = await firebase.messaging().getToken();
+  //     if (fcmToken) {
+  //       // user has a device token
+  //       await AsyncStorage.setItem('fcmToken', fcmToken);
+  //     }
+  //   }
+  // }
 
   changeChecked = () => {
     this.setState({checked: !this.state.checked});
@@ -44,6 +86,11 @@ class SignUpScene extends Component {
       selected: value,
     });
   };
+
+  beforeSubmit() {
+    // this.getToken();
+    console.log(this.state);
+  }
 
   render() {
     return (
@@ -82,20 +129,27 @@ class SignUpScene extends Component {
               placeholderIconColor="#007aff"
               selectedValue={this.state.selected}
               onValueChange={this.changeValue.bind(this)}>
-              <Picker.Item label="Patient" value="key0" />
-              <Picker.Item label="Caretaker " value="key1" />
-              <Picker.Item label="Doctor" value="key2" />
+              <Picker.Item label="Patient" value="patient" />
+              <Picker.Item label="Caretaker " value="caretaker" />
+              <Picker.Item label="Doctor" value="doctor" />
             </Picker>
           </Item>
 
           <Item inlineLabel>
             <Icon style={styles.icon} name="user" />
-            <Input placeholder="Username" />
+            <Input
+              placeholder="Username"
+              onChangeText={(value) => this.setState({username: value})}
+            />
           </Item>
 
           <Item inlineLabel>
             <Icon style={styles.icon} name="phone-square" />
-            <Input keyboardType="number-pad" placeholder="Phone Number" />
+            <Input
+              keyboardType="number-pad"
+              placeholder="Phone Number"
+              onChangeText={(value) => this.setState({phoneNumber: value})}
+            />
           </Item>
 
           <Item inlineLabel>
@@ -103,6 +157,7 @@ class SignUpScene extends Component {
             <Input
               secureTextEntry={this.state.secureEntry}
               placeholder="Password"
+              onChangeText={(value) => this.setState({password: value})}
             />
             <Button
               transparent
@@ -123,6 +178,7 @@ class SignUpScene extends Component {
             <Input
               secureTextEntry={this.state.secureEntry}
               placeholder="Re-Enter Password"
+              onChangeText={(value) => this.setState({reEnterPassword: value})}
             />
           </Item>
 
@@ -133,11 +189,8 @@ class SignUpScene extends Component {
             />
             <Body>
               <Text style={styles.subText}>
-                I Agree to the{' '}
-                <Text
-                  onPress={() => {
-                    this.setState({modalVisible: true});
-                  }}
+                I Agree to the
+                <Text onPress={() =>this.setState({modalVisible: true})}
                   style={styles.link}>
                   Terms and Conditions
                 </Text>
@@ -149,8 +202,7 @@ class SignUpScene extends Component {
             primary
             block
             style={styles.button}
-            onPress={()=>console.log(this.state)}
-            >
+            onPress={() => this.beforeSubmit()}>
             <Text style={styles.buttonText}>Sign Up</Text>
           </Button>
         </Form>
