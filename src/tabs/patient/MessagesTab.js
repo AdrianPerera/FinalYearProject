8613fae648a64f0d177545d172438fa04282cc58
AdrianPerera <nikiperera20@gmail.com>
2021-04-12 01:53:18 +0530

    NIT : merge changes

--- src/tabs/patient/MessagesTab.js
+++ src/tabs/patient/MessagesTab.js
@@ -1,40 +1,92 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Container,
   Header,
   Body,
   Title,
-  Content,
-  Card,
-  CardItem,
-  Badge,
   Right,
   Left,
   Text,
   Button,
 } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Plotly from 'react-native-plotly';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-function MessagesTab({navigation}) {
-  return (
-    <Container>
-      <Header>
-        <Left>
-          <Button
-            style={{width: 50, justifyContent: 'center'}}
-            onPress={() => navigation.toggleDrawer()}>
-            <Icon name="reorder" style={{fontSize: 30, color: 'white'}} />
-          </Button>
-        </Left>
-        <Body>
-          <Title style={{textTransform: 'capitalize'}}>Messages</Title>
-        </Body>
-        <Right>
-          <Icon name="envelope" style={{fontSize: 30, color: '#e4e213'}} />
-        </Right>
-      </Header>
-    </Container>
-  );
+function MessagesTab({ navigation }) {
+
+  const [data, setData] = useState([]);
+  const [layout, setLayout] = useState({ title: 'Ucler Pressure Map' });
+
+  const fetchDetails = async () => {
+    try {
+      const token = await AsyncStorage.getItem('@auth_token');
+      if (token === null) {
+        console.log('token null');
+      }
+      console.log("token " + token);
+      getData(token)
+    } catch (e) {
+      console.log("token error: " + e);
+    }
+  }
+
+  const getData = async (token) => {
+
+    var headers = new Headers();
+    headers.append("Authorization", "Token " + token);
+    headers.append("Content-Type", "application/json");
+    var requestOption = {
+      method: 'GET',
+      headers: headers,
+    };
+
+    await fetch('http://prevelcer.herokuapp.com/pressure/visualize?patient=Adrian', requestOption)
+      .then((response) => response.json())
+      .then((result) => {
+        setData(result.image)
+      }).catch((error) => console.log('Data list ', error));
+
+  }
+  const data1 = [
+    {
+      z: data,
+      type: 'heatmap',
+      colorscale: 'Jet',
+      zmin: 0,
+      zmax: 700,
+      zsmooth: 'best'
+    }
+  ];
+
+  const update = (_, { data, layout, config }, plotly) => {
+    plotly.react(data, layout, config);
+  };
+
+  useEffect(() => {
+    fetchDetails();
+  }, [])
+
+  function MessagesTab({ navigation }) {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button
+              style={{ width: 50, justifyContent: 'center' }}
+              onPress={() => navigation.toggleDrawer()}>
+              <Icon name="reorder" style={{ fontSize: 30, color: 'white' }} />
+            </Button>
+          </Left>
+          <Body>
+            <Title style={{ textTransform: 'capitalize' }}>Messages</Title>
+          </Body>
+          <Right>
+            <Icon name="envelope" style={{ fontSize: 30, color: '#e4e213' }} />
+          </Right>
+        </Header>
+      </Container>
+    );
+  }
 }
-
 export default MessagesTab;
